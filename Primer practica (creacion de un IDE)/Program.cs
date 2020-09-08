94618ab2fed3d0f7765345f87b14556cfdcf6f7e
Gerson Reynoso <ger12xela@gmail.com>
2020-09-07 22:38:38 -0600

    Agregar archivos de proyecto.

---
+++ Primer practica (creacion de un IDE)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primer_practica__creacion_de_un_IDE_
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
