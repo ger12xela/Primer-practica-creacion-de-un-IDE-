dfeb865b4686b032170bbec49cba939900f6b1b2
Gerson Reynoso <ger12xela@gmail.com>
2020-09-09 15:49:52 -0600

    guardar y abrir archivos .gt

--- Primer practica (creacion de un IDE)/Form1.Designer.cs
+++ Primer practica (creacion de un IDE)/Form1.Designer.cs
@@ -68,6 +68,7 @@
             this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
             this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
             // 
             // menuAbrir
             // 
@@ -81,6 +82,7 @@
             this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
             this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
             // 
             // editarToolStripMenuItem
             // 
--- Primer practica (creacion de un IDE)/Form1.cs
+++ Primer practica (creacion de un IDE)/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,7 @@ namespace Primer_practica__creacion_de_un_IDE_
 {
     public partial class Form1 : Form
     {
+        String archivo;
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +22,67 @@ namespace Primer_practica__creacion_de_un_IDE_
 
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            OpenFileDialog abrirArchivo = new OpenFileDialog();
+            abrirArchivo.Filter = "Archivo de IDE | *.gt";
 
+
+            if (abrirArchivo.ShowDialog() == DialogResult.OK)
+            {
+                archivo = abrirArchivo.FileName;
+                using (StreamReader leer = new StreamReader(archivo)) {
+
+                    richTextBox1.Text = leer.ReadToEnd();
+                }
+            }
+
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+            Console.WriteLine(richTextBox1.Text);
+        }
 
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog guardarArchivo = new SaveFileDialog();
+            guardarArchivo.Filter = "Archivo de IDE | *.gt";
+
+            if (archivo != null)
+            {
+                using (StreamWriter escribir = new StreamWriter(archivo))
+                {
+                    escribir.Write(richTextBox1.Text);
+                }
+            }
+            else
+            {
+                if (guardarArchivo.ShowDialog() == DialogResult.OK)
+                {
+                    archivo = guardarArchivo.FileName;
+                    using (StreamWriter escribir = new StreamWriter(guardarArchivo.FileName))
+                    {
+                        escribir.Write(richTextBox1.Text);
+                    }
+                }
+            }
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != null)
+            {
+            if (MessageBox.Show("Los cambios no guardados se perderan", "Confimar Nuevo", MessageBoxButtons.YesNo)==DialogResult.Yes) 
+            {
+                    richTextBox1.Clear();
+                    archivo = null;
+            }
+
+            }
+            else
+            {
+                MessageBox.Show("La hoja esta vacia", "Informacion"); 
+            }
         }
     }
 }
